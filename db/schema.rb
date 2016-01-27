cd68851450b13ae178d668f3d38f1ce8f497db9e
kyonya <info@saradnentika.net>
2016-01-27 22:46:03 +0900

    create user page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120134326) do
+ActiveRecord::Schema.define(version: 20160127130138) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20160120134326) do
     t.string   "place"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "twitter_id"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
