4c0baee7db07c5c6efe2d550fe421da350edc723
gennei <sai.gennei@gmail.com>
2016-03-07 22:04:47 +0900

    validate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, length: { maximum: 15 }
+
+  validates :twitter_id, presence: true
+  validates :twitter_id, length: { maximum: 15 }
+
+  validates :message, presence: true
+  validates :icon, presence: true
 end
